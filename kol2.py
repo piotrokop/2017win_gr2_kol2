4b9f7ded2f4bc54f1d301519054e927b7435ac6f
Piotr Pabian <4pabian@taurus.fis.agh.edu.pl>
2017-11-16 17:10:29 +0100

    init

--- kol2.py
+++ kol2.py
@@ -13,3 +13,55 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+class Course():
+	def __init__(self, name):
+		self.name = name
+		self.scores = []
+
+	def setScore(self, score):
+		self.scores.append(score)
+
+	def getScores(self):
+		return self.scores
+
+	def getAverage(self):
+		return sum(self.scores)/len(self.scores)
+
+class Diary():
+	def __init__(self, name, surname):
+		self.name = name
+		self.surname = surname
+		self.courses = {}
+
+	def setCourse(self, name):
+		self.courses[name] = Course(name)
+
+	def setScore(self, courseName, score):
+		self.courses[courseName].setScore(score)
+
+	def getCourseAverage(self, course):
+		return self.courses[course].getAverage()
+
+	def getTotalAverage(self):
+		sum = 0
+		for course in self.courses.values():
+			sum += course.getAverage()
+		return sum/len(self.courses)
+
+
+if __name__=="__main__":
+	diary = Diary('Piotr', 'Pabian')
+	diary.setCourse('PIPE')
+	diary.setCourse('Math')
+	diary.setScore('PIPE', 5)
+	diary.setScore('PIPE', 2)
+	diary.setScore('PIPE', 5)
+	diary.setScore('PIPE', 4)
+	diary.setScore('Math', 5)
+	diary.setScore('Math', 2.5)
+	diary.setScore('Math', 4.5)
+	diary.setScore('Math', 4.5)
+	print diary.getCourseAverage('PIPE')
+	print diary.getCourseAverage('Math')
+	print diary.getTotalAverage()
